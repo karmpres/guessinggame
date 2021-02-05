0b1fc24e34c5aa9848242429df1bf080066e5447
karmpres <kpresent75@gmail.com>
2021-02-05 12:16:28 -0600

    GuessingGame

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+// This is basically a random number guessing game.
+// There will be two players. First they will guess their number and then the number will be compared to see who is closer. The closer one wins.
+// lets try no use class on this one, then the other one will use class.
+
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int PlayerANum, PlayerBNum;
+
+                Console.WriteLine("Player A, please guess a number from 1 to 10");
+                PlayerANum = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Player B, please guess a number from 1 to 10");
+                PlayerBNum = Convert.ToInt32(Console.ReadLine());
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
